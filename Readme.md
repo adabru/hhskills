23855761542f92066f1071105b938fd046545386
adabru <b.brunnmeier@gmx.de>
2020-10-11 14:12:12 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+
+## Development
+
+```
+# copy code
+git clone git@github.com:adabru/infoaghh.git
+cd infoaghh
+
+# start server
+node server.js ::1 8080
+
+# now that the server is running, open your browser at http://[::1]:8080/
+```
---
+++ server.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const contenttypes = require('./contenttypes.json')
+
+help = () => console.log(`
+
+\x1b[1musage\x1b[0m: server.js iface port
+
+`)
+
+iface = process.argv[2]
+port = process.argv[3]
+if (!iface || !port)
+  return help()
+
+function route(path) {
+  switch (true) {
+    case path == '/':
+      return './index.html'
+    case /^\/[\d][\d]W[\d][\d]$/.test(path):
+      return `./blog${path}/index.html`
+    default:
+      return `.${path}`
+  }
+}
+
+let server = http.createServer( (req, res) => {
+  req.url = unescape(req.url)
+  switch(true) {
+    case req.url != path.normalize(req.url):
+      res.writeHead(400)
+      res.end('invalid path')
+      break
+    case req.url == '/websites':
+      fs.readdir('../educational-webhosting/public', (err, files) => {
+        if (err) {
+          res.writeHead(500)
+          res.end('server error')
+        } else {
+          res.writeHead(200)
+          res.end(JSON.stringify(files.filter(f => f != 'index.html' && f != 'test')))
+        }
+      })
+      break
+    default:
+      filepath = route(req.url)
+      fs.createReadStream(filepath)
+        .on('error', (e) => {
+          console.error(e)
+          res.writeHead(404)
+          res.end('not found')
+        }).on('open', () => {
+          res.writeHead(200, {'content-type': (contenttypes[path.extname(filepath)] || 'text/plain')})
+        }).pipe(res)
+  }
+})
+
+server.listen(port, iface, () => console.log(`server running on http://${iface}:${port}`))
+
+server.on('error', e => {
+  console.log('An error occured while serving: ' + e)
+  setTimeout(() => {
+      server.close();
+      server.listen(config.port, config.host);
+    }, 1000)
+})
+
+process.on('SIGINT', function () {
+  try { server.close() }
+  catch(e) { }
+  process.exit()
+})
